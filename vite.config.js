7ffc1e09216fad2f5f62caa64fbd53e12f63819d
omprakashsao <saoomprakash2002@gmail.com>
2025-04-09 17:08:00 +0530

    remove console statements

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 import path from "path"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
+import removeConsole from 'vite-plugin-remove-console';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), removeConsole()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
